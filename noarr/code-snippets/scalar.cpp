69893fb0fd4db357f8b1e874793b8677dbf02704
Adam Smelko <smelko@d3s.mff.cuni.cz>
2024-02-14 11:40:11 +0100

    finally add papers and unify the structure

--- noarr/code-snippets/scalar.cpp
+++
@@ -1,6 +0,0 @@
-template<class T>
-struct scalar : contain<> {
-	static constexpr size_t size() noexcept { return sizeof(T); }
-	static constexpr size_t offset() noexcept { return 0; }
-	static constexpr size_t length() noexcept { return 0; }
-};
--- noarr/code-snippets/stencil_base.cpp
+++
@@ -1,6 +0,0 @@
-template <typename... Args> void run_stencil_grid(Args&&... args) {
-    for (size_t x = 1; x < size_x - 1; x++)
-        for (size_t y = 1; y < size_y - 1; y++)
-            for (size_t z = 1; z < size_z - 1; z++)
-                stencil(std::forward<Args>(args)..., x, y, z);
-}
